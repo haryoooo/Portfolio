47a96d4d10b9b831d15a7133252b657b38f6923e
haryo <haryo.novianto@mandalafinance.com>
2023-10-14 12:04:44 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { collection, getDocs } from "firebase/firestore";
+import TheWelcome from "../components/TheWelcome.vue";
+import { db } from "../firebase";
+import { onMounted } from "vue";
+
+onMounted(async () => {
+  const querySnapshot = await getDocs(collection(db, "dataList"));
+  querySnapshot.forEach((el) => {
+    console.log(el.data());
+  });
+});
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
